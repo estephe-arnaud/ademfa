679587b09eb86d2956c73aec100d764ef7fc005c
estephe-arnaud <estephe.arnaud@gmail.com>
2021-03-19 19:24:20 +0100

    Update init scripts

--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -1,7 +1,1 @@
-DEFAULT_PARAMS = {
-  "project_name": "face_alignment",
-  "model_name": "t_MoE",
-  "results_dir": "./output/face_alignment",
-  "seed": 42,
-  "verbose": True
-}
+import mapping
--- facial_expression_recognition/__init__.py
+++ facial_expression_recognition/__init__.py
@@ -1,7 +1,1 @@
-DEFAULT_PARAMS = {
-  "project_name": "facial_expression_recognition",
-  "model_name": "VGGFace",
-  "results_dir": "./output/facial_expression_recognition",
-  "seed": 42,
-  "verbose": True
-}
+import mapping
